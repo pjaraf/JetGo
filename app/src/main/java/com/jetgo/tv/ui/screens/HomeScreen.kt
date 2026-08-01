b2dcd9dc01cd396993887b52d4d4a37ac4c99f0e
pjaraf <143246360+pjaraf@users.noreply.github.com>
2026-08-01 17:05:23 +0000

    Corregir import faltante de remember en HomeScreen

--- app/src/main/java/com/jetgo/tv/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/jetgo/tv/ui/screens/HomeScreen.kt
@@ -25,6 +25,7 @@ import androidx.compose.material.icons.outlined.Face
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
