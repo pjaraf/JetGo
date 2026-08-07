becb6593ed99a0059c2ac56820e1b3dd20eedf03
pjaraf <143246360+pjaraf@users.noreply.github.com>
2026-08-07 18:10:11 +0000

    Agregar imports que faltaban (height, Column)

--- app/src/main/java/com/jetgo/tv/ui/components/FullscreenPlayerOverlay.kt
+++ app/src/main/java/com/jetgo/tv/ui/components/FullscreenPlayerOverlay.kt
@@ -3,7 +3,9 @@ package com.jetgo.tv.ui.components
 import androidx.compose.foundation.background
 import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
